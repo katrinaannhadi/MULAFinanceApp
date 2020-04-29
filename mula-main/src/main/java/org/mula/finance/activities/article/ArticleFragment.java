5c35d4ab620fb1971d4b44246b206f38dff200ed
Katrina Ann Hadi <katrinaann.abdulhadi@student.unsw.edu.au>
2020-04-29 17:49:47 +1000

    -

--- mula-main/src/main/java/org/mula/finance/activities/article/ArticleFragment.java
+++ mula-main/src/main/java/org/mula/finance/activities/article/ArticleFragment.java
@@ -86,6 +86,8 @@ public class ArticleFragment extends Fragment {
                 .placeholder(R.drawable.image_investment)
                 .centerCrop()
                 .into(image);
+
+
         search.setOnClickListener(new View.OnClickListener() {
 
             @Override
